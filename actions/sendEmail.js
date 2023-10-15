2b0f7d650889aaa92ef6fe79b58d769bad0e8aaa
Pushpak Jain <84683252+pushpakjn@users.noreply.github.com>
2023-10-15 22:23:42 +0530

    Update sendEmail.js

--- actions/sendEmail.js
+++ actions/sendEmail.js
@@ -2,7 +2,8 @@
 
 import { Resend } from "resend";
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+// const resend = new Resend(process.env.RESEND_API_KEY);
+const resend = new Resend('re_CUCfS5Gy_LqFEgu2QQYnF6t4U8UEH7RrA');
 
 const sendEmail = async (FormData) => {
   const senderEmail = FormData.get("senderEmail");
